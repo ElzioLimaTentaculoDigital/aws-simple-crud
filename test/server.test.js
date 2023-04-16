d9812b59b22d7db797a5a6d7894e498b971ffa4c
ElzioLimaTentaculoDigital <elzio.lima@tentaculo.digital>
2023-04-16 19:17:33 -0400

    finished project

---
+++ test/server.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const Server = require('../lib/server-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/server-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new Server.ServerStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
